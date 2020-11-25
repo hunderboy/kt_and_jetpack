ad6c15bf74c632b843b2d1ba19f4a06d6d185cbf
hunderboy <wheeing@naver.com>
2020-11-25 10:49:07 +0900

    // 구조에 대한 이해 에 따른 주석

--- app/src/main/java/kr/co/everex/kotlin_and_jectpack/MainActivity.kt
+++ app/src/main/java/kr/co/everex/kotlin_and_jectpack/MainActivity.kt
@@ -22,14 +22,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // 데이터 바인딩 적용 build.gradle Module 수준
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        binding.lifecycleOwner = this // 라이브 데이터 때문에 하는 것.
+        // 라이브 데이터 때문에 하는 것. = live data 의 라이프사이클 인식을 위해서
+        binding.lifecycleOwner = this
 
+        // 뷰모델 선언
+        // AndroidViewModel 사용 할 경우 viewmodel 객체 설정
         val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        binding.viewModel = viewModel
+        binding.viewModel = viewModel // 뷰모델 연결
 
-
-
+        /**
+         * 아래 해당 코드 연산 처리는
+         * Data binding 을 통해 Xml 쪽으로 넘긴다.
+         * 그럼으로써 메인Activity 의 코드 량을 줄여준다.
+         *
+        1. observe 패턴 을 사용한 변경된 데이터 인식 변경
+        전체 결과 텍스트
+        android:text="@{viewModel.todos.toString()}"
+         *
+        2. 버튼 클릭을 통한 데이터 처리
+        EditText 에서
+        android:text="@={viewModel.newTodo}"
+         *
+        추가 버튼에서
+        android:onClick="@{() -> viewModel.insert(viewModel.newTodo)}"
+         */
         // 데이터 관찰 가능하게 observe 패턴 사용
 //        viewModel.getAll().observe(this, Observer {
 //            // it을 활용 하던가, todos ->
--- app/src/main/java/kr/co/everex/kotlin_and_jectpack/MainViewModel.kt
+++ app/src/main/java/kr/co/everex/kotlin_and_jectpack/MainViewModel.kt
@@ -16,13 +16,18 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         AppDatabase::class.java, "database-name"
     ).build()
 
+    // Todo 객체를 포함하는 List 형태의 LiveData
     var todos : LiveData<List<Todo>>
+
+    // 추가할 Text
+    // 추가 버튼 누를 시에 EditText 의 입력된 Text 가 newTodo 에 할당됨
+    // 할당된 Text가 MainViewModel 의  insert 함수를 통해 viewModelScope(ViewModel 비동기 처리를 통해 처리됨)
     var newTodo: String? = null
 
     init {
         todos = getAll()
     }
-
+    // 데이터 전체 추출
     fun getAll(): LiveData<List<Todo>> {
         return db.todoDao().getAll()
     }
@@ -30,9 +35,11 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     // suspend : 난 무조건 코루틴 안에서 비동기로 처리되어야 한다
     fun insert(todo: String) {
 //        db.todoDao().insert(todo)
+
+        //Todo Room 접근은 비동기 처리를 통해 접근해야 한다.
         viewModelScope.launch(Dispatchers.IO) {
             db.todoDao().insert(Todo(todo))
         }
-
     }
+
 }
